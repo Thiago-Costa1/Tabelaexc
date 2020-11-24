307283becc616769b352c16aa9f2e5c45a9abaa3
Thiago <thiagocostas2017@gmail.com>
2020-11-24 17:34:22 -0300

    completo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exc5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             {
+             Console.WriteLine("Tabela de multiplicação");
+             Console.WriteLine();
+             Console.WriteLine("Escreva um número de 1 a 10:\n ");
+             int numero = Convert.ToInt16(Console.ReadLine());
+             for (int num=0;num<11;num++)
+            {   int resultado=num*numero;
+                Console.WriteLine();
+                Console.WriteLine($"{num}x{numero}={resultado}");}
+            }
+        }
+    }
+}
