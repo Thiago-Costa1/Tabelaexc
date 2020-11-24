9faf94a950f9264043c6b10b8f691a7f466c70e2
Thiago <thiagocostas2017@gmail.com>
2020-11-24 17:35:31 -0300

    pequenas atualizações feitas

--- Program.cs
+++ Program.cs
@@ -6,13 +6,12 @@ namespace Exc5
     {
         static void Main(string[] args)
         {
-             {
-             Console.WriteLine("Tabela de multiplicação");
+             {Console.WriteLine("Tabela de multiplicação");
              Console.WriteLine();
              Console.WriteLine("Escreva um número de 1 a 10:\n ");
              int numero = Convert.ToInt16(Console.ReadLine());
              for (int num=0;num<11;num++)
-            {   int resultado=num*numero;
+            {int resultado=num*numero;
                 Console.WriteLine();
                 Console.WriteLine($"{num}x{numero}={resultado}");}
             }
